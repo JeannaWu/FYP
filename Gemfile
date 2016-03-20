7819fa75baa52442dff516f020a2fe594bb2f5fb
ZHIYUN WU <wzyunzx@163.com>
2016-03-20 20:11:57 +0000

    Change gemfile for heroku

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
-
+ruby '2.2.1'
     gem 'rails',        '4.2.2'
     gem 'sass-rails',   '5.0.2'
-    gem 'mysql2', '~> 0.3.18'
+   
+   
+
     gem 'bcrypt-ruby', '3.1.2' 
     gem 'uglifier',     '2.5.3'
     gem 'coffee-rails', '4.1.0'
@@ -10,7 +12,8 @@ source 'https://rubygems.org'
     gem 'sdoc',         '0.4.0', group: :doc
 
     group :development, :test do
-      gem 'sqlite3',     '1.3.9'
+      #gem 'sqlite3',     '1.3.9'
+       gem 'mysql2', '~> 0.3.18'
       gem 'byebug',      '3.4.0'
       gem 'web-console', '2.0.0.beta3'
       gem 'spring',      '1.1.3'
@@ -26,7 +29,7 @@ source 'https://rubygems.org'
     gem 'bcrypt',   '3.1.7'
     gem 'faker',                '1.4.2'
     gem 'simple_form', '~> 3.2'
-    gem 'will_paginate',           '3.0.7'
+    gem "will_paginate", ">=3.0.pre4"
     gem 'bootstrap-will_paginate', '0.0.10'
     gem 'time_ago_in_words', '~> 0.1.1'
     
@@ -40,10 +43,11 @@ source 'https://rubygems.org'
      gem 'acts_as_follower', '~> 0.2.1'
      gem 'acts_as_list', '~> 0.7.2'
      gem 'cancancan', '~> 1.10'
-     gem 'paper_trail'
+     gem 'paper_trail', '~> 4.1'
+     gem 'kaminari', '~> 0.16.3'
     gem 'rails_admin', '~> 0.8.1'
      gem 'role_model', '~> 0.8.2'
-     
+     gem 'state_machines'
 
      
     
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308190746) do
+ActiveRecord::Schema.define(version: 20160316095753) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -57,11 +57,19 @@ ActiveRecord::Schema.define(version: 20160308190746) do
     t.datetime "image_updated_at"
     t.integer  "club_id",            limit: 4
     t.integer  "category_id",        limit: 4
+    t.integer  "type_id",            limit: 4
+    t.datetime "approved_at"
   end
 
   create_table "search_suggestions", force: :cascade do |t|
     t.string   "term",       limit: 255
     t.integer  "popularity", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
@@ -90,21 +98,34 @@ ActiveRecord::Schema.define(version: 20160308190746) do
     t.text     "introduction",           limit: 65535
     t.boolean  "admin",                  limit: 1
     t.integer  "roles_mask",             limit: 4
+    t.string   "panel",                  limit: 255
+    t.boolean  "paneluser",              limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "version_associations", force: :cascade do |t|
+    t.integer "version_id",       limit: 4
+    t.string  "foreign_key_name", limit: 255, null: false
+    t.integer "foreign_key_id",   limit: 4
+  end
+
+  add_index "version_associations", ["foreign_key_name", "foreign_key_id"], name: "index_version_associations_on_foreign_key", using: :btree
+  add_index "version_associations", ["version_id"], name: "index_version_associations_on_version_id", using: :btree
+
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",  limit: 255,        null: false
-    t.integer  "item_id",    limit: 4,          null: false
-    t.string   "event",      limit: 255,        null: false
-    t.string   "whodunnit",  limit: 255
-    t.text     "object",     limit: 4294967295
+    t.string   "item_type",      limit: 255,        null: false
+    t.integer  "item_id",        limit: 4,          null: false
+    t.string   "event",          limit: 255,        null: false
+    t.string   "whodunnit",      limit: 255
+    t.text     "object",         limit: 4294967295
     t.datetime "created_at"
+    t.integer  "transaction_id", limit: 4
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
+  add_index "versions", ["transaction_id"], name: "index_versions_on_transaction_id", using: :btree
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id",   limit: 4
